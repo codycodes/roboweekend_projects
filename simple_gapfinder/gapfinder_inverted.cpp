b72afa30129d0f4ed249c7b13e821a4a1f1a6366
cody.codes <codygagnon@gmail.com>
2018-01-07 01:44:48 +0000

    refactored the code

--- simple_gapfinder/gapfinder_inverted.cpp
+++ simple_gapfinder/gapfinder_inverted.cpp
@@ -7,66 +7,62 @@
 #include <geometry_msgs/Twist.h>
 
 #include<stdio.h>
+#include<vector>
+
+using namespace std;
 
 ros::Publisher cmdVelPub;
+
+struct Gap {
+	int startIndex;
+	int endIndex;
+};
+
+std::vector<Gap> findGaps(const sensor_msgs::LaserScan::ConstPtr& msg, float threshold){
+
+	vector<Gap> gaps;
+
+	Gap temp;
+	bool start = false;
+	float range = 0.0f;
+
+	for(int i=0; i<msg->ranges.size(); i++){
+		range = msg->ranges[i];
+		if(range >= msg->range_min && range <= msg->range_max){
+			if (!start && range >= threshold) {
+				start = true;
+				temp.startIndex = i;
+				ROS_INFO("starting at %i", i);
+			}
+
+			if(start && range < threshold){
+				ROS_INFO("ending at %i", i);
+				start = false;
+				temp.endIndex = i;
+				gaps.push_back(temp);
+			}
+		}
+	}
+
+	if(start){
+		temp.endIndex = msg->ranges.size() - 1;
+		ROS_INFO("ending at %i END", temp.endIndex);
+		gaps.push_back(temp);
+	}
+
+	return gaps;
+}
 
 void laserCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
 	ROS_INFO("Got %i laser scans", msg->ranges.size());
 
-	float angle = msg->angle_min;
-	for(int i=0; i<msg->ranges.size(); i++){
-		float range = msg->ranges[i];
-		int max_thresh = msg->range_max;
-		if(range >= msg->range_min && range <= msg->range_max){
-			bool start = false;
-			bool end = false;
-			int start_idx = 0;
-			int end_idx = 0;
-			while (range >= max_thresh) {
-				if (start != true) {
-					start = true;
-					start_idx = i;
-				}
-				if (i >= msg->ranges.size()) {
-					end_idx = range;
-					end = true;
-					break;
-			 }
+	vector<Gap> gaps = findGaps(msg, 2.0f);
 
-				i = i + 1;
-				range = msg->ranges[i];
-				if (range < max_thresh) {
-					end_idx = i;
-					end = true;
-					break;
-				}
-		  }
-				}
-			}
-			// print out the range if we're at the end
-			if (end == true) {
-				ROS_INFO("[%i, %i]", start_idx, end_idx);
-			}
-		}
 
-		angle += msg->angle_increment;
+	for(int i=0; i<gaps.size(); i++){
+		Gap gap = gaps[i];
+		ROS_INFO("[%i, %i]", gap.startIndex, gap.endIndex);
 	}
-
-	geometry_msgs::Twist cmd;
-	if(!obstacle){
-		ROS_INFO("SAFE");
-
-		cmd.linear.x = 0.5;
-		cmd.angular.z = 0;
-	}
-	else{
-		ROS_INFO("TURN");
-
-		cmd.linear.x = 0;
-		cmd.angular.z = 1.0;
-	}
-
-	cmdVelPub.publish(cmd);
 }
 
 
